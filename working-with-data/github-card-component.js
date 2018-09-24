9aef99d404d59ca0a9bb7c3913f18b2de5250df2
David Dinh <dmdinh22@gmail.com>
2018-09-24 00:46:25 -0700

    create CardList component to render multiple card components

--- working-with-data/github-card-component.js
+++ working-with-data/github-card-component.js
@@ -1,13 +1,27 @@
 const Card = props => {
     return (
         <div>
-            <img src="http://placehold.it/75" />
-            <div>
-                <div>Name here</div>
-                <div>Company Name here</div>
+            <img width="75" src={props.avatar_url} />
+            <div style={{ display: 'inline-block', marginLeft: 10 }}>
+                <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>
+                    {props.name}
+                </div>
+                <div>{props.company}</div>
             </div>
         </div>
     );
 };
 
-ReactDOM.render(<Card />, mountNode);
+const CardList = props => {
+    return (
+        <div>
+            <Card
+                name="Paul O'Shannessy"
+                avatar_url="https://avatars.githubusercontent.com/u/8445?v=3"
+                company="Facebook"
+            />
+        </div>
+    );
+};
+
+ReactDOM.render(<CardList />, mountNode);
