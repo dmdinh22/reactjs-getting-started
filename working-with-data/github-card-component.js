11b38abdc3e0c94fc19680974d9d19ce1b4639d4
David Dinh <dmdinh22@gmail.com>
2018-09-24 00:51:41 -0700

    map props into card components

--- working-with-data/github-card-component.js
+++ working-with-data/github-card-component.js
@@ -12,16 +12,30 @@ const Card = props => {
     );
 };
 
+let data = [
+    {
+        name: "Paul O'Shannessy",
+        avatar_url: 'https://avatars.githubusercontent.com/u/8445?v=3',
+        company: 'Facebook'
+    },
+    {
+        name: 'Ben Alpert',
+        avatar_url:
+            'https://avatars3.githubusercontent.com/u/7585659?s=460&v=4',
+        company: 'Facebook'
+    }
+];
+
 const CardList = props => {
+    // using spread operator to spread the card object with all
+    // the properties of card object available for card component
     return (
         <div>
-            <Card
-                name="Paul O'Shannessy"
-                avatar_url="https://avatars.githubusercontent.com/u/8445?v=3"
-                company="Facebook"
-            />
+            {props.cards.map(card => (
+                <Card {...card} />
+            ))}
         </div>
     );
 };
 
-ReactDOM.render(<CardList />, mountNode);
+ReactDOM.render(<CardList cards={data} />, mountNode);
