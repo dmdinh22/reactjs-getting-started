3f765bf6036abdd04247c9ff8b1512e8b1b88201
David Dinh <dmdinh22@gmail.com>
2018-09-23 22:44:22 -0700

    making the components reusable

---
+++ Introduction/reusable-components.js
@@ -0,0 +1,56 @@
+class Button extends React.Component {
+    handleClick = () => {
+        // this === component instance
+        this.props.onClickFunction(this.props.incrementValue);
+    };
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                +{this.props.incrementValue}
+            </button>
+        );
+    }
+}
+
+const Result = props => {
+    return <div>{props.counter}</div>;
+};
+
+class App extends React.Component {
+    state = { counter: 0 };
+
+    incrementCounter = incrementValue => {
+        // this === component instance
+        this.setState(prevState => ({
+            counter: prevState.counter + incrementValue
+        }));
+    };
+
+    render() {
+        //pass the reference to the incrementCounter function into the Button component props
+        return (
+            <div>
+                <Button
+                    incrementValue={1}
+                    onClickFunction={this.incrementCounter}
+                />
+                <Button
+                    incrementValue={5}
+                    onClickFunction={this.incrementCounter}
+                />
+                <Button
+                    incrementValue={10}
+                    onClickFunction={this.incrementCounter}
+                />
+                <Button
+                    incrementValue={100}
+                    onClickFunction={this.incrementCounter}
+                />
+                <Result counter={this.state.counter} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, mountNode);
