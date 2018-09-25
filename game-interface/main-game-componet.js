aaef8f44be27428f248f68f35fd410c39383aa40
David Dinh <dmdinh22@gmail.com>
2018-09-24 23:00:14 -0700

    add styling to components

--- game-interface/main-game-componet.js
+++ game-interface/main-game-componet.js
@@ -1,8 +1,51 @@
+const Stars = props => {
+    return (
+        <div className="col-5">
+            <i className="fa fa-star" />
+            <i className="fa fa-star" />
+            <i className="fa fa-star" />
+            <i className="fa fa-star" />
+        </div>
+    );
+};
+
+const Button = props => {
+    return (
+        <div className="col-2">
+            <button>=</button>
+        </div>
+    );
+};
+
+const Answer = props => {
+    return <div className="col-5">...</div>;
+};
+
+const Numbers = props => {
+    return (
+        <div className="card text-center">
+            <div>
+                <span>1</span>
+                <span className="selected">2</span>
+                <span className="used">3</span>
+            </div>
+        </div>
+    );
+};
+
 class Game extends React.Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <h3>Play Nine</h3>
+                <hr />
+                <div className="row">
+                    <Stars />
+                    <Button />
+                    <Answer />
+                </div>
+                <br />
+                <Numbers />
             </div>
         );
     }
