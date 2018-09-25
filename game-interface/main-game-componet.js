c29cadae79c086c1fba0a167e07cc89eb962f8e2
David Dinh <dmdinh22@gmail.com>
2018-09-24 23:03:54 -0700

    randomize number of stars

--- game-interface/main-game-componet.js
+++ game-interface/main-game-componet.js
@@ -1,12 +1,11 @@
 const Stars = props => {
-    return (
-        <div className="col-5">
-            <i className="fa fa-star" />
-            <i className="fa fa-star" />
-            <i className="fa fa-star" />
-            <i className="fa fa-star" />
-        </div>
-    );
+    const numberOfStars = 1 + Math.floor(Math.random() * 9);
+
+    let stars = [];
+    for (let i = 0; i < numberOfStars; i++) {
+        stars.push(<i key={i} className="fa fa-star" />);
+    }
+    return <div className="col-5">{stars}</div>;
 };
 
 const Button = props => {
